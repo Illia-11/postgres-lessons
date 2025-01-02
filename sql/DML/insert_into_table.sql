ce2dedea1ebc5192c2742c35e95074749859ce03
Illia Piddubnyi <illya2008zp@gmail.com>
2025-01-02 21:00:15 +0200

    Add INSERT examples

---
+++ sql/DML/insert_into_table.sql
@@ -0,0 +1,14 @@
+-- INSERT INTO - команда для вставки записів у таблицю
+--- вставка 1 запису
+INSERT INTO users VALUES 
+('John', 'Doe', 'jd@gmail.com', 9999.99, true, '2001-04-15');
+--- вставка декількох записів
+INSERT INTO users VALUES
+('Jane', 'Smith', 'js@gmail.com', 1234.99, false, '1999-05-13'),
+('Test', 'Test', 'test@gmail.com', 8.99, true, '1998-11-09'),
+('Admin', 'Moder', 'am@gmail.com', 3456.54, true, '1035-12-23');
+-- вставка даних у довільному порядку і у вказані стовпчики
+INSERT INTO users
+(account_balance, birthday, first_name, last_name, email, is_male)
+VALUES
+(5000, '1550-04-25', 'Vlad', 'Tcepes', 'vt@gmail.com', true);
