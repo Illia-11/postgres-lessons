5393feb4ca561435c750532941095dc5294cdfd9
Illia Piddubnyi <illya2008zp@gmail.com>
2025-01-02 19:44:30 +0200

    Add SQL sublanguages

---
+++ sql/sql_sublanguages.sql
@@ -0,0 +1,20 @@
+/*
+  Команди SQL поділяється на певні "категорії" які називаються підмовами (sublanguages)
+
+  DDL (Data Defenition Language) - мова визначення даних
+    використовується для опису структури даних
+      команди розпочинаються з ключових слів CREATE, DROP, ALTER, TRUNCATE
+
+  DML (Data Manipulation Language) - мова маніпуляцій даних
+    використовується для зміниї даних БД
+      команди розпочинаються з ключових слів INSERT, UPDATE, DELETE
+
+  DQL (Data Query Language) - мова запитів даних
+    використовується для отримання даних з БД
+      SELECT
+
+  DCL (Data Control Language) - мова контролю даних
+    повʼязане з адмініструванням
+
+  TCL (Transaction Control Language) - мова контролю транзакцій
+*/
