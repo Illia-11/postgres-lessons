7b78e5461b9b92fff7cd10afa8756f1025bb44e0
Illia Piddubnyi <illya2008zp@gmail.com>
2025-01-07 18:51:00 +0200

    Add single user insertion example

--- script/index.js
+++ script/index.js
@@ -10,13 +10,36 @@ const config = {
 
 const client = new Client(config);
 
-async function startScript() {
+const user = {
+  firstName: "Insert",
+  lastNamr: "Insertovich",
+  email: "inserter@gmail.com",
+  accountBalance: 250,
+  height: 1.67,
+  isMale: true,
+  birthday: "1980, 7, 15",
+};
 
+async function startScript() {
   await client.connect();
 
-  const res = await client.query("SELECT * FROM users");
+  const res = await client.query(`
+    INSERT INTO users (
+      first_name,
+      last_name,
+      email,
+      is_male,
+      height,
+      account_balance,
+      birthday
+    ) VALUES
+    ('${user.firstName}', '${user.lastName}', '${user.email}', ${user.isMale}, ${user.height}, ${user.accountBalance}, '${user.birthday}')
+    RETURNING *; 
+  `);
 
-  console.log(res); // res.rows - результати запиту (користувачі)
+  // console.log(res); // res.rows - результати запиту (користувачі)
+
+  console.log(res.rows[0]);
 
   await client.end();
 }
