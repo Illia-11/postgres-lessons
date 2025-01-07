d775d8946a5d325295e0b0e01ddba53a52edf478
Illia Piddubnyi <illya2008zp@gmail.com>
2025-01-07 19:02:44 +0200

    Add multiple users insertion example

--- script/index.js
+++ script/index.js
@@ -12,13 +12,55 @@ const client = new Client(config);
 
 const user = {
   firstName: "Insert",
-  lastNamr: "Insertovich",
+  lastName: "Insertovich",
   email: "inserter@gmail.com",
   accountBalance: 250,
   height: 1.67,
   isMale: true,
-  birthday: "1980, 7, 15",
+  birthday: "1980-07-15",
 };
+
+const users = [
+  {
+    firstName: "Insert 1",
+    lastName: "Insertovich 1",
+    email: "inserter1@gmail.com",
+    accountBalance: 250,
+    height: 1.67,
+    isMale: true,
+    birthday: "1980-07-15",
+  },
+  {
+    firstName: "Insert 2",
+    lastName: "Insertovich 2",
+    email: "inserter2@gmail.com",
+    accountBalance: 250,
+    height: 1.67,
+    isMale: false,
+    birthday: "1995-02-12",
+  },
+  {
+    firstName: "Insert 3",
+    lastName: "Insertovich 3",
+    email: "inserter3@gmail.com",
+    accountBalance: 250,
+    height: 1.67,
+    isMale: true,
+    birthday: "1976-07-15",
+  },
+];
+
+function mapUser() {
+  return `('${user.firstName}', '${user.lastName}', '${user.email}', ${user.isMale}, ${user.height}, ${user.accountBalance}, '${user.birthday}')`;
+}
+
+const usersInsertStringsArray = users.map(mapUser);
+
+// console.log(usersInsertStrings);
+
+const usersInsertStrings = usersInsertStringsArray.join(",");
+
+// console.log(usersInsertStrings);
 
 async function startScript() {
   await client.connect();
@@ -33,7 +75,7 @@ async function startScript() {
       account_balance,
       birthday
     ) VALUES
-    ('${user.firstName}', '${user.lastName}', '${user.email}', ${user.isMale}, ${user.height}, ${user.accountBalance}, '${user.birthday}')
+    ${usersInsertStrings}
     RETURNING *; 
   `);
 
